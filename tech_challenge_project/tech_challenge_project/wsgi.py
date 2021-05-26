6b6f0efd08dcc989147595094a636a785f97c2a9
Adil Reza <adilreza043@gmail.com>
2021-05-27 02:38:39 +0600

    initializing the dango project setup

---
+++ tech_challenge_project/tech_challenge_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tech_challenge_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_challenge_project.settings')
+
+application = get_wsgi_application()
