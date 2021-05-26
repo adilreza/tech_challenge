5fec66ab36269bd57caf4ce6f1a9751b4496182d
Adil Reza <adilreza043@gmail.com>
2021-05-27 02:48:47 +0600

    adding new app and changing settings file in the project

---
+++ tech_challenge_project/tech_challenge_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TechChallengeAppConfig(AppConfig):
+    name = 'tech_challenge_app'
--- tech_challenge_project/tech_challenge_project/settings.py
+++ tech_challenge_project/tech_challenge_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tech_challenge_app',
 ]
 
 MIDDLEWARE = [
