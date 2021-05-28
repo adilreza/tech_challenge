33a4a36966a3709e3256d8a85573d54b7bbb6e27
Adil Reza <adilreza043@gmail.com>
2021-05-28 16:09:23 +0600

    solving all 1 issue

--- tech_challenge_project/tests/test_models.py
+++ tech_challenge_project/tests/test_models.py
@@ -50,24 +50,29 @@ class ModelTestCase(APITestCase):
 
         self.assertEqual(response.status_code, 404)
 
-    # def test_match(self):
-    #     data = {"first_name":"adil", "last_name":"reza","province":"Ok","dat_of_birth":"1997-01-12", "alt_first_name":"leo"}
-    #     url = "/api/v1/notice/"
-    #     response1 = self.client.post(url, data)
-    #     url2 = f"/api/v1/record/{response1.data['id']}"
-    #     response11 = self.client.get(url2)
+    def test_match(self):
+        data = {"first_name":"adil", "last_name":"reza","province":"ON","dat_of_birth":"1997-01-12", "alt_first_name":"leo"}
+        url = "/api/v1/notices/"
+        self.response1 = self.client.post(url, data)
+        url2 = f"/api/v1/notices/{self.response1.data['id']}"
+        print(url2)
+        response11 = self.client.get(url2)
 
-    #     data = {"first_name":"adil", "last_name":"reza","province":"Ok","dat_of_birth":"1997-01-12"}
-    #     url = "/api/v1/record/"
-    #     response2 = self.client.post(url, data)
-    #     url3 = f"/api/v1/record/{response2.data['id']}"
-    #     response22 = self.client.get(url3)
-    #     print(response22)
+        data = {"first_name":"adil", "last_name":"reza","province":"Ok","dat_of_birth":"1997-01-12"}
+        url = "/api/v1/records/"
+        self.response2 = self.client.post(url, data)
+        url3 = f"/api/v1/records/{self.response2.data['id']}"
+        response22 = self.client.get(url3)
 
-    #     url = "/api/v1/match/"
-    #     data = {"notice":response11.data, "record":response22.data, "type":1}
+        url = "/api/v1/matches/"
+        data33 = {"notice":response11, "record":response22, "type":1}
 
-    #     response = self.client.post(url, data)
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post(url, data33)
+        self.assertEqual(response.status_code, 405)
+
+    def test_match_404(self):
+        url = "/api/v1/matc"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
 
 
